f3eb8c5d1f0cbeddb1c07ae3ad24232f59bf7ff9
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-10-22 11:46:51 +0200

    update

---
+++ Desarrollo Entorno Servidor/Introducion php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+        print "<u><b>Hola que tal </b></u>";
+        echo "<br>Segunda linea";
+        echo "<br>tercera linea ";
+    ?>
+</body>
+
+</html>
