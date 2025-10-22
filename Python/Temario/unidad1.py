8deaf429d22ea87843c49006fcc5d6b667654069
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-10-22 19:45:19 +0200

    update

---
+++ Python/Temario/unidad1.py
@@ -0,0 +1,18 @@
+# primer print 
+print("Ejecucion correcta")
+print("")
+
+""" Formatos de salida """
+nombre = "Miguel"
+print(f"Formateando la salida de consola con f-string por parte de {nombre}")
+
+print("Añadiendo correo y contando con lend")
+correo = "asasf@gmail.com"
+print(f"El correo de {nombre} el correo es {correo}, el cual cuenta con {len(correo)} caracteres")
+
+print("Los input")
+nombre = input("¿Cuál es tu nombre? ")
+correo = input("¿Cuál es tu correo? ")
+edad = input("¿Cuál es tu edad? ")
+
+print(f"el nombre: {nombre}")
