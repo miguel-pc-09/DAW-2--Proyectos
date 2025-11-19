e62b60f10640d1e09a63e45fc0632d421ec16acd
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-11-19 18:22:34 +0100

    update

---
+++ Desarrollo Entorno Servidor/Evaluables/1º Trimestre/ejercicio1.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Ejercicio 1</title>
+</head>
+
+<body>
+    <?php 
+        // Ejercicio 1. Realizar un programa PHP que generé 2 números aleatorios (entre 5 y 15) y me dibujé un rectángulo de asteriscos como se puesta en la figura:
+        echo "<h1>Generacion de rectangulos gracias a números aleatorios. </h1>";
+
+        $filas = rand(5, 15);     // número aleatorio de filas 
+        $columnas = rand(5,15);   // número aleatorio de columnas
+
+
+
+        // utilizamos 2 for para que genere el rectangulo dependiendo de los numeros aleatorios que saque
+        echo "Alto: $filas <br>";
+        echo "Ancho: $columnas <br>";
+
+        for ($i=0; $i < $filas; $i++) {             // bucle para filas
+            for ($j=0; $j < $columnas;  $j++) {     // bucle para columnas
+                echo " * ";                 // añade asteriscos
+            }
+            echo"<br>";    // salto de linea al terminar
+        }
+
+
+    ?>
+</body>
+
+</html>
