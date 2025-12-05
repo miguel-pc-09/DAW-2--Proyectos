7cdc0847935e063e53ecc6c35f9235aad81ac983
miguel-pc-09 <m.peruchacastello@gmail.com>
2025-12-05 18:02:39 +0100

    update

---
+++ Python/Ejercicios/01_Tema2/00_gestion_lista.py
@@ -0,0 +1,11 @@
+""" 
+Crea un programa en Python que permita gestionar una lista de productos de una tienda. 
+El programa debe utilizar funciones para realizar las siguientes operaciones: 
+    - Agregar productos con su nombre y precio
+    - Mostrar todos los productos disponibles 
+    - Buscar un producto por su nombre y mostrar su precio 
+    - Calcular el precio total de todos los productos
+Organiza tu código en funciones separadas para cada una de estas acciones. El usuario debe poder elegir que hacer mediente un menú simple.
+Además, tendras que utilizar estructuras de control como listas para poder almacenar todos los productos.
+
+"""
