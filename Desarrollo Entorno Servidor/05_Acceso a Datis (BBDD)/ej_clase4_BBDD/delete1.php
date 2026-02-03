ac438c5c350232dcd664f0f4af72087dc1f4c5ae
miguel-pc-09 <m.peruchacastello@gmail.com>
2026-02-03 10:32:53 +0100

    update

---
+++ Desarrollo Entorno Servidor/05_Acceso a Datis (BBDD)/ej_clase4_BBDD/delete1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Borrar Producto</title>
+</head>
+<body>
+    <h1>Borrar Producto</h1>
+    <form method="post" action="delete2.php">
+        <label for="id">ID del Producto:</label>
+        <input type="number" name="id" required>
+        <input type="submit" value="Borrar">
+    </form>
+    <a href="indice.php">Volver al menú</a>
+</body>
+</html>
---
+++ Desarrollo Entorno Servidor/05_Acceso a Datis (BBDD)/ej_clase4_BBDD/indice.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Menú Principal</title>
+</head>
+<body>
+    <h1>Menú Principal</h1>
+    <ul>
+        <li><a href="add1.php">Añadir Producto</a></li>
+        <li><a href="list.php">Listar Productos</a></li>
+        <li><a href="search1.php">Buscar Producto</a></li>
+        <li><a href="delete1.php">Borrar Producto</a></li>
+    </ul>
+</body>
+</html>
