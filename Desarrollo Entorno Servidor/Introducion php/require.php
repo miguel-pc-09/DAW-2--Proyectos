18717796f2d4b31a1cc3970fbb41b02420fe1bc3
miguel-pc-09 <m.peruchacastello@gmail.com>
2026-03-20 19:00:16 +0100

    update

---
+++ Desarrollo Entorno Servidor/Introducion php/require.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php 
+        // Incluir biblioteca de funciones
+    require ("conecta.php");
+    require ("fecha.php");
+    require ("cadena.php");
+    require ("globals.php");    
+    ?>
+</head>
+
+<body>
+    <?php 
+        include ("cabecera.html");
+    ?>
+    <!-- 
+     // Código HTML + PHP
+     ......
+      -->
+    <?php 
+        include ("pie.html");
+    ?>
+</body>
+
+</html>
+<!--  Ambos incluyen y evalúan el fichero especidficado-->
