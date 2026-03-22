0d3d48de5bf8749007117492ad783a4978886d36
miguel-pc-09 <m.peruchacastello@gmail.com>
2026-03-22 13:52:30 +0100

    update

--- Desarrollo Entorno Servidor/Ejercicios/01_tema2/Ejemplos ejercicios/ej_while2.php
+++ Desarrollo Entorno Servidor/Ejercicios/01_tema2/Ejemplos ejercicios/ej_while2.php
@@ -5,5 +5,5 @@ while ( $numero > 1 ) {
    $factorial = $factorial * $numero;
    $numero = $numero - 1;
 }
-echo ' El factorial de ' . $numero . ' es ' . $factorial.
+echo ' El factorial de '.$numero.' es '.$factorial;
 ?>
